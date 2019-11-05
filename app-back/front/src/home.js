d16e74a9993c0d576a64e2cebdbf617e6028994d
cristian <cc.pena@uniandes.edu.co>
2019-11-04 23:29:55 -0500

    back

--- app-back/front/src/home.js
+++ app-back/front/src/home.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Route, Switch,Link } from 'react-router-dom';
 import { Navbar, Nav} from 'react-bootstrap';
 import Inicio from './inicio';
+import Taller from './Taller/taller';
 
 class Home extends Component {
     
@@ -55,6 +56,9 @@ class Home extends Component {
                  <Route exact path='/' >
 
                    <Inicio auth={this.props.auth}></Inicio>
+                 </Route>
+                 <Route path="/talleres">
+                   <Taller></Taller>
                  </Route>
             </Switch>
         </div>
