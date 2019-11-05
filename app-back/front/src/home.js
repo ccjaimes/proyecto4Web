7fef32f4724112216940236fca793c6eac006c6d
lala341 <li.forero@uniandes.edu.co>
2019-11-04 23:04:49 -0500

    Cambios auth0

--- app-back/front/src/home.js
+++ app-back/front/src/home.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Route, Switch,Link } from 'react-router-dom';
 import { Navbar, Nav} from 'react-bootstrap';
 import Inicio from './inicio';
-import Taller from './Taller/taller'
 
 class Home extends Component {
     
