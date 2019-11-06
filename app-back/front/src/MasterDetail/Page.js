00a857218b391383213389868f80a2eb8b554ab0
lala341 <li.forero@uniandes.edu.co>
2019-11-06 08:54:50 -0500

    Cambios

--- app-back/front/src/MasterDetail/Page.js
+++ app-back/front/src/MasterDetail/Page.js
@@ -8,6 +8,8 @@ import Reportar from '../reportar';
 import Grupo from '../Grupo/grupo';
 import User from '../User/User';
 import ListRobos from '../listRobos';
+import Taller from '../Taller/taller';
+import TallerDetailed from '../Taller/tallerDetailed';
 
 
 class Page extends Component {
@@ -71,7 +73,13 @@ class Page extends Component {
                                     </ListGroup.Item>
 
                                 </Link>
+                                <Link to="/talleres" style={{ color: '#D90429' }}>
 
+<ListGroup.Item action className="List__Group"  >
+    <h5 style={{ color: '#D90429' }}>Talleres</h5>
+</ListGroup.Item>
+
+</Link>
 
                                 <ListGroup.Item action className="List__Group" >
                                     <NavLink onClick={this.logout.bind(this)} style={{ color: '#D90429' }}><h5 style={{ color: '#D90429' }}>Cerrar Sesión</h5></NavLink>
@@ -80,17 +88,17 @@ class Page extends Component {
                         </Col>
                         <Col xs={12} md={9} lg={9} className="col-8 App__Form">
                             <Switch>
-                                <Route exact path='/page/' ><User auth={this.props.auth}></User></Route>
+                                <Route exact path='/page/' render ={(props)=><User auth={this.props.auth} {...props}/>}></Route>
 
-                                <Route path='/user'  ><User auth={this.props.auth}></User></Route>
-                                <Route path='/reportar' auth={this.props.auth} >
-                                    <Reportar></Reportar>
+                                <Route path='/user' render ={(props)=><User auth={this.props.auth} {...props}/>}></Route>
+                                <Route exact path='/talleres'  render ={(props)=><Taller auth={this.props.auth} {...props}/>}></Route>
+                                <Route exact path = '/talleres/:id' render ={(props)=><TallerDetailed auth={this.props.auth} {...props}/>}/>            
+                                
+                                <Route path='/reportar' render ={(props)=><Reportar auth={this.props.auth} {...props}/>}>
                                 </Route>
-                                <Route path="/grupos">
-                                    <Grupo></Grupo>
+                                <Route path="/grupos" render ={(props)=><Grupo auth={this.props.auth} {...props}/>}>
                                 </Route>
-                                <Route path="/robos">
-                                    <ListRobos></ListRobos>
+                                <Route path="/robos" render ={(props)=><ListRobos auth={this.props.auth} {...props}/>}>
                                 </Route>
                             </Switch>
                         </Col>
